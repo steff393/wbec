e5651a08e0143bd3185f9ef9f3df35cc31ed1bff
steff393 <stefan.ferstl@gmail.com>
2021-04-11 15:51:40 +0200

    Add basic input via Serial

--- src/main.cpp
+++ src/main.cpp
@@ -31,41 +31,53 @@ uint16_t resultB[34];   // Bus-Adr. 100-133
 uint16_t resultC[4];    // Bus-Adr. 200-203
 uint16_t resultD[19];   // Bus-Adr. 300-318
 uint16_t resultE[320];  // Bus-Adr. 500-819
-uint8_t cReg = 0;
+uint8_t msgCnt = 0;
+char buffer[40];
+uint16_t WdTime_Readv = 0;
+uint16_t WdTime_Readn = 0;
+uint16_t WdTime_Write = 15000;
 
 void loop() {
-  startTime = millis();
-  if (!mb.slave()) {
-    switch(cReg++) {
-      case 0:
-        mb.readIreg(1, 4,   resultA, 15, cbWrite);
-      break;
-      case 1:
-        mb.readIreg(1, 100, resultB, 25, cbWrite);
-      break;
-      case 2:
-        mb.readIreg(1, 200, resultC,  4, cbWrite);
-      break;
-      default:
-        for (int i = 0; i < 15 ; i++) {
-          yield();
-          Serial.print(i);Serial.print(" ");Serial.println(resultA[i]);
-        }
-        for (int i = 0; i < 34 ; i++) {
-          yield();
-          Serial.print(i);Serial.print(" ");Serial.println(resultB[i]);
-        }
-        for (int i = 0; i < 4 ; i++) {
-          yield();
-          Serial.print(i);Serial.print(" ");Serial.println(resultC[i]);
-        }
-        Serial.println(millis()-startTime);
-        
-        delay(5000);
-        cReg = 0;
+  
+
+  int key = Serial.read();
+  Serial.print("%; Pressed: "); Serial.println(key); 
+
+  if (millis()-startTime > 10000) {
+
+    if (!mb.slave()) {
+      switch(msgCnt++) {
+        case 0: mb.readIreg(1, 4,   resultA, 15, cbWrite); break;
+        case 1: mb.readIreg(1, 100, resultB, 25, cbWrite); break;
+        case 2: mb.readIreg(1, 125, &resultB[25], 9, cbWrite); break;
+        case 3: mb.readIreg(1, 200, resultC,  4, cbWrite); break;
+        case 4: mb.readHreg(1, 257, &WdTime_Readv,  1, cbWrite); break;
+        //case 5: mb.writeHreg(1, 257, &WdTime_Write,  1, cbWrite); break;
+        case 6: mb.readHreg(1, 257, &WdTime_Readn,  1, cbWrite); break;
+        default:
+          for (int i = 0; i < 15 ; i++) {
+            Serial.print(i);Serial.print(" ");Serial.println(resultA[i]);
+          }
+          //for (int i = 0; i < 34 ; i++) {
+            //Serial.print(i);Serial.print(" ");Serial.printf_P("Resp: %s\n", (char*) resultB);
+          //  Serial.print(i);Serial.print(" ");Serial.println(resultB[i]);
+          //}
+          //Serial.print("Test");Serial.print(" ");Serial.println((char*) resultB);
+          //strcpy(buffer, (char *) result);
+          //Serial.print("Test");Serial.print(" ");Serial.println(buffer);
+          for (int i = 0; i < 4 ; i++) {
+            Serial.print(i);Serial.print(" ");Serial.println(resultC[i]);
+          }
+          Serial.print("vorher ");Serial.print(" ");Serial.println(WdTime_Readv);
+          Serial.print("Write..");Serial.print(" ");Serial.println(WdTime_Write);
+          Serial.print("nachher");Serial.print(" ");Serial.println(WdTime_Readn);
+          Serial.println(millis()-startTime);
+          
+          startTime = millis();
+          msgCnt = 0;
+      }
+
     }
-
-
     //mb.readIreg(1, 4,   resultA, 15, cbWrite);
     //mb.readIreg(1, 100, resultB, 2, cbWrite);
     //mb.readIreg(1, 200, resultC,  4, cbWrite);
@@ -74,6 +86,6 @@ void loop() {
 
   }
   mb.task();
-  //delay(500);
   yield();
+  delay(500);
 }
