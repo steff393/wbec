5614598474d58d95b3392f27fc7d259b2279ed20
lk3de <24473659+lk3de@users.noreply.github.com>
2024-01-21 17:37:51 +0100

    Extend MQTT password length to 127 chars

--- src/globalConfig.h
+++ src/globalConfig.h
@@ -36,7 +36,7 @@ extern uint16_t cfgFailsafeCurrent;          // <don't use - still beta> Reg. 26
 extern char     cfgMqttIp[16];               // IP address of MQTT broker, "" to disable MQTT
 extern uint16_t cfgMqttPort;                 // Port of MQTT broker (optional)
 extern char     cfgMqttUser[32];             // MQTT: Username
-extern char     cfgMqttPass[32];             // MQTT: Password
+extern char     cfgMqttPass[128];            // MQTT: Password
 extern uint8_t  cfgMqttLp[WB_CNT];           // Array with assignments to openWB loadpoints, e.g. [4,2,0,1]: Box0 = LP4, Box1 = LP2, Box2 = no MQTT, Box3 = LP1
 extern char     cfgMqttWattTopic[60];        // MQTT: Topic for setting the watt value for PV charging, default: "wbec/pv/setWatt"
 extern char     cfgMqttWattJson[30];         // MQTT: Optional: Element in a JSON string, which contains the power in watt, default: ""
